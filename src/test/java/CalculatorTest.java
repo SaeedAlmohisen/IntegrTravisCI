fa9c742aed112dbc748f71a72b9baa031a3be109
eng.salqarni92@gmail.com <eng.salqarni92@gmail.com>
2019-12-03 16:16:17 -0500

    success test1

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -4,10 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
 
-    @Test
-    void test1() {
-        assertEquals(2, Calculator.add(5, 6));
-    }
+
 
     @Test
     void test2(){
